ce2edf9f86d611141c67cbb063dbd5da81e0be9f
Roger <roger7a7@gmail.com>
2021-02-04 18:42:38 +0100

    ANIMATIONS

--- RodriguezMendezRoger_MuñozBessererJordi_P0/main/Click.go
+++ RodriguezMendezRoger_MuñozBessererJordi_P0/main/Click.go
@@ -45,7 +45,7 @@ embedded_components {
   "mask: \"Cursor\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
   "      x: 0.0\n"
   "      y: 0.0\n"
@@ -58,11 +58,9 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 3\n"
+  "    count: 1\n"
   "  }\n"
-  "  data: 75.0\n"
-  "  data: 75.0\n"
-  "  data: 10.0\n"
+  "  data: 63.5675\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
@@ -71,60 +69,6 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "PointClickObj"
-  type: "label"
-  data: "size {\n"
-  "  x: 128.0\n"
-  "  y: 20.0\n"
-  "  z: 0.0\n"
-  "  w: 0.0\n"
-  "}\n"
-  "scale {\n"
-  "  x: 1.0\n"
-  "  y: 1.0\n"
-  "  z: 1.0\n"
-  "  w: 0.0\n"
-  "}\n"
-  "color {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "outline {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "shadow {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "leading: 1.0\n"
-  "tracking: 0.0\n"
-  "pivot: PIVOT_CENTER\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  "line_break: false\n"
-  "text: \"-AQUI ANIRA UNA BARRA DEL TEMPS-\"\n"
-  "font: \"/builtins/fonts/system_font.font\"\n"
-  "material: \"/builtins/fonts/label.material\"\n"
-  ""
-  position {
-    x: 0.0
-    y: -106.0
     z: 0.0
   }
   rotation {
